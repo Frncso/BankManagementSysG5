0f7e6a6dc6c812565e49421c5288216528e0b144
b4pmain <ezekielfrancisco10@gmail.com>
2026-03-06 13:16:05 +0800

    Added Necessary Classes for Banking System (to be assigned)

--- src/BankManage/Main.java
+++ src/BankManage/Main.java
@@ -1,33 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package BankManage;
 
-import java.util.Scanner;
-
-/**
- *
- * @author Ezekiel
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        String fname;
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter your First Name: ");
-        fname = sc.nextLine();
-        
-        if(fname.equalsIgnoreCase("melvin")){
-            System.out.println("langkiwa");
-        }
-        else{
-            System.out.println("Hello, "+fname+"!");
-        }
+
     }
     
 }
---
+++ src/BankManage/Transaction.java
@@ -0,0 +1,4 @@
+package BankManage;
+public class Transaction {
+    
+}
