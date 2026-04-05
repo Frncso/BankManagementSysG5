625c53f2dd482c7e91495b75e780169f26d0567f
AtheaRodriguez22 <rodriguezatheairish22@gmail.com>
2026-04-05 20:50:21 +0800

    Updated Login UI and Dashboard and Fixed some errors
    Co-Authored-By: Frncso <265800198+Frncso@users.noreply.github.com>
    Signed-off-by: AtheaRodriguez22 <rodriguezatheairish22@gmail.com>

---
+++ src/BankManage/CustomerDashboard.java
@@ -0,0 +1,18 @@
+package BankManage;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author athea
+ */
+public class CustomerDashboard extends JFrame {
+
+  public CustomerDashboard() {
+      setTitle("Customer Dashboard");
+      setSize(1440, 960);
+      setLayout(null);
+      
+  }}
+      
+   
--- src/BankManage/LoginUI.java
+++ src/BankManage/LoginUI.java
@@ -4,8 +4,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-public class LoginUI extends JFrame implements ActionListener {
+public class LoginUI extends JFrame  implements ActionListener {
    
+    public static void main(String[] args) {
+        ///// nag-eensures run sa same thread
+        SwingUtilities.invokeLater(() -> {
+            LoginUI login = new LoginUI();
+            login.setVisible(true);
+        });
+    }
     ColorScheme cs = new ColorScheme();
 
     private CardLayout loginSwitch;
@@ -48,8 +55,6 @@ public class LoginUI extends JFrame implements ActionListener {
         btnStf = new JButton("Staff");
         btnStf.setBounds(220,40,175,30);
         btnStf.setBackground(cs.btnColorUnselect);
-//        btnStf.setForeground(cs.);
-//        btnStf.setBorderPainted(false);
 
         JLabel lblAcc = new JLabel("Account Number");
         lblAcc.setBounds(45,100,200,30);
@@ -60,7 +65,6 @@ public class LoginUI extends JFrame implements ActionListener {
 
         JLabel lblPin = new JLabel("PIN");
         lblPin.setBounds(45,180,200,30);
-//        lblPin.setForeground(cs.);
 
         txtPin = new JPasswordField("1234");
         txtPin.setBounds(45,210,350,35);
@@ -79,7 +83,6 @@ public class LoginUI extends JFrame implements ActionListener {
         btnRegister.setBounds(240,320,120,30);
         btnRegister.setBorderPainted(false);
         btnRegister.setContentAreaFilled(false);
-//        btnRegister.setForeground(cs.bgColor);
 
         loginCus.add(btnCus);
         loginCus.add(btnStf);
@@ -91,26 +94,18 @@ public class LoginUI extends JFrame implements ActionListener {
         loginCus.add(lblBottom);
         loginCus.add(btnRegister);
 
-//        loginCus.setBackground(cs.);
-
-        // for cus panel
+        // for CUS panel
         btnCusS = new JButton("Customer");
         btnCusS.setBounds(45,40,175,30);
-//        btnCusS.setBackground(cs.btnColorUnselect);
-//        btnCusS.setForeground(cs.);
         btnCusS.setBorderPainted(false);
         
         // for staff panel
         btnStfS = new JButton("Staff");
         btnStfS.setBounds(220,40,175,30);
-//        btnStfS.setBackground(cs.btnColorSelect);
-//        btnStfS.setForeground(cs.);
         btnStfS.setBorderPainted(false);
 
         btnSignInS = new JButton("Sign In");
         btnSignInS.setBounds(45,270,350,35);
-//        btnSignInS.setBackground(cs.btnColorSelect);
-//        btnSignInS.setForeground(cs.);
         btnSignInS.setBorderPainted(false);
 
         JLabel lblBottomS = new JLabel("Don't have an account?");
@@ -129,8 +124,6 @@ public class LoginUI extends JFrame implements ActionListener {
         loginStf.add(lblBottomS);
         loginStf.add(btnRegisterS);
 
-//        loginStf.setBackground(cs.);
-
         loginContainer.setBounds(500,250,445,400);
         add(loginContainer);
 
@@ -142,69 +135,57 @@ public class LoginUI extends JFrame implements ActionListener {
         btnSignInS.addActionListener(this);
     }
 
-    @Override
+   @Override
     public void actionPerformed(ActionEvent e){
 
         if(e.getSource()==btnStf){
-
-//            btnStf.setBackground(cs.btnColorSelect);
-//            btnStf.setForeground(cs.);
-//
-//            btnCus.setBackground(cs.btnColorUnselect);
-//            btnCus.setForeground(cs.);
-
             loginSwitch.show(loginContainer,"staff");
         }
 
         else if(e.getSource()==btnCusS){
-
-//            btnCus.setBackground(cs.btnColorSelect);
-//            btnCus.setForeground(cs.);
-
             btnStf.setBackground(cs.btnColorUnselect);
-//            btnStf.setForeground(cs.);
-
             loginSwitch.show(loginContainer,"customer");
         }
-
+        //for CUS access
         else if (e.getSource() == btnSignIn) {
-             String accNo = txtAccNo.getText().trim();
-            String pin = new String(txtPin.getPassword()).trim();
-
-            if (accNo.isEmpty() || pin.isEmpty()) {
-                JOptionPane.showMessageDialog(this,
-                    "Please enter your Account Number and PIN.",
-                    "Login Error", JOptionPane.WARNING_MESSAGE);
-                return;
-            }
-
-            // validate Cus
-            JOptionPane.showMessageDialog(this, "Customer login not yet implemented.");
-
-        } else if (e.getSource() == btnSignInS) {
-
             String accNo = txtAccNo.getText().trim();
             String pin = new String(txtPin.getPassword()).trim();
 
-            if (accNo.isEmpty() || pin.isEmpty()) {
-                JOptionPane.showMessageDialog(this,
-                    "Please enter your Account Number and PIN.",
-                    "Login Error", JOptionPane.WARNING_MESSAGE);
-                return;
+            //Pass ng CUS
+            if (accNo.equals("12345") && pin.equals("1111")) {
+                JOptionPane.showMessageDialog(this, "Login Successful! Welcome, Customer.");
+                CustomerDashboard dash = new CustomerDashboard(); 
+                dash.setVisible(true);
+    
+             //CLOSE
+             dispose(); 
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Customer Account or PIN.", "Login Error", JOptionPane.ERROR_MESSAGE);
             }
 
-            //validate stf 
-            JOptionPane.showMessageDialog(this, "Login for staff not yet implemented.");
+        } 
+        
+        //for STF
+        else if (e.getSource() == btnSignInS) {
+            String accNo = txtAccNo.getText().trim();
+            String pin = new String(txtPin.getPassword()).trim();
+
+            //Pass ng STF
+            if (accNo.equals("admin") && pin.equals("admin123")) {
+                JOptionPane.showMessageDialog(this, "Login Successful! Welcome, Admin.");
+                
+              //where/what? call for admin inter
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Staff Credentials.", "Login Error", JOptionPane.ERROR_MESSAGE);
+            }
 
         } else if (e.getSource() == btnRegister || e.getSource() == btnRegisterS) {
-
             RegisterUI rg = new RegisterUI();
             rg.setVisible(true);
             dispose();
-     }
+        }
   }}
 
-//NOTE: may possible changes pa 
 //  1.purpleDark = Color(0x5B21B6);
 //  2.purple  = Color(0x7C3AED);
 //  3.purpleLight = Color(0xA78BFA);
